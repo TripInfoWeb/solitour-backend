f18daf9e150fce74208528e7cbef4fa020276a35
Donghun Won <wonhun1225@gmail.com>
2024-10-20 14:26:52 +0900

    Refactor : cors 설정 yml로 관리하도록 변경 (#237)

--- src/main/java/solitour_backend/solitour/auth/config/AuthConfiguration.java
+++ src/main/java/solitour_backend/solitour/auth/config/AuthConfiguration.java
@@ -2,6 +2,8 @@ package solitour_backend.solitour.auth.config;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,6 +16,10 @@ import solitour_backend.solitour.auth.support.JwtTokenProvider;
 @RequiredArgsConstructor
 @Configuration
 public class AuthConfiguration implements WebMvcConfigurer {
+
+
+    @Autowired
+    private CorsProperties corsProperties;
 
     private final JwtTokenProvider jwtTokenProvider;
     private final TokenRepository tokenRepository;
@@ -40,16 +46,10 @@ public class AuthConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        final String[] ALLOWED_URLS = {
-                "http://localhost:3000",
-                "https://solitour.ssssksss.xyz",
-                "https://solitour-admin.ssssksss.xyz"
-        };
-
         registry.addMapping("/**")
-                .allowedOrigins(ALLOWED_URLS)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
+                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
+                .allowedMethods(corsProperties.getAllowedMethods().toArray(new String[0]))
+                .allowedHeaders(corsProperties.getAllowedHeaders().toArray(new String[0]))
                 .allowCredentials(true);
     }
 }
